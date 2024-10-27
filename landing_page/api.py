abdbc0d9e7e6293b07f4a52ef7fbf5fdb2b3772c
chrisprod06 <chrisprodromou06@gmail.com>
2024-10-27 13:51:01 +0200

    contact us message api completed

--- landing_page/api.py
+++ landing_page/api.py
@@ -16,10 +16,6 @@ def create_client_registration(request, data: CreateClientRegistrationSchema):
     try:
         # Attempt to create the client registration
         client_registration = ClientRegistration.objects.create(**data.model_dump())
-        print(client_registration.id)
-        print(client_registration.email)
-        print(client_registration.date_registered)
-        print(f"Client registration created!")
         # Successful creation
         return 201, {"message": "Client Registered successfully!", "email": client_registration.email}
 
@@ -39,7 +35,20 @@ def create_client_registration(request, data: CreateClientRegistrationSchema):
         return 400, {"message": "An unexpected error occurred: " + str(e)}
 
 
-@router.post("/contact_us_message", response=ContactUsMessageSchema)
+@router.post("/contact_us_message",
+             response={201: ContactUsMessageSchema,
+                       400: ErrorResponseSchema,
+                       405: ErrorResponseSchema,})
 def create_contact_us_message(request, data: CreateContactUsMessageSchema):
-    contact_us_message = ContactUsMessage.objects.create(**data.model_dump())
-    return contact_us_message
+    try:
+        contact_us_message = ContactUsMessage.objects.create(**data.model_dump())
+        return 201, {"message": "Contact Us Message Created!", "email": contact_us_message.email}
+    except ValueError as e:
+        # Bad request: Data validation issues
+        print(str(e))
+        return 400, {"message": str(e)}
+    except Exception as e:
+        # Generic error or unhandled cases
+        print(str(e))
+        return 400, {"message": "An unexpected error occurred: " + str(e)}
+
--- landing_page/schemas.py
+++ landing_page/schemas.py
@@ -3,15 +3,8 @@ from datetime import date
 from typing import Literal
 
 class ContactUsMessageSchema(BaseModel):
-    id: int
-    first_name: str
-    last_name: str
     email: EmailStr
-    phone_number: str
-    subject_inquiry: Literal["GI", "BI", "CI"]  # Enum for inquiry choices
     message: str
-    status: Literal["PE", "PR", "RE", "CO"]  # Enum for status choices
-    date_submitted: date
 
     class Config:
         from_attributes = True
